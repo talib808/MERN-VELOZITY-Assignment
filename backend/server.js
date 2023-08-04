acc79570c801f95d54add4e4f0c24b3291c1030a
Md Talib Ansari <95065465+talib808@users.noreply.github.com>
2023-08-04 12:45:18 +0530

    All Done

---
+++ backend/server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+
+
+app.get('/api/movies/search', async (req, res) => {
+  const { query } = req.query;
+  try {
+    const response = await axios.get(
+      `http://www.omdbapi.com/?apikey=881cd60a&s=${query}`
+    );
+    res.json(response.data.Search || []);
+  } catch (error) {
+    console.error('Error searching movies:', error);
+    res.status(500).json({ error: 'An error occurred while searching movies' });
+  }
+});
+
+const favorites = [];
+
+app.post('/api/movies/favorites', (req, res) => {
+  const { imdbID } = req.body;
+
+  if (!imdbID) {
+    return res.status(400).json({ error: 'imdbID is required' });
+  }
+
+  const existingIndex = favorites.findIndex((fav) => fav.imdbID === imdbID);
+
+  if (existingIndex !== -1) {
+    favorites.splice(existingIndex, 1);
+    res.json({ message: 'Removed from favorites' });
+  } else {
+    favorites.push({ imdbID });
+    res.json({ message: 'Added to favorites' });
+  }
+});
+
+app.get('/api/movies/favorites', (req, res) => {
+  res.json(favorites);
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
