c4990d24866095ef803dd197c017be5302ac3c76
Md Talib Ansari <95065465+talib808@users.noreply.github.com>
2023-08-08 01:47:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,8 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// app.use(cors(
-// {
-// origin: [" "],
-// methods: ["POST", "GET"],
-// credentials: true
-// }
-// ));
- app.use(cors());
+
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
