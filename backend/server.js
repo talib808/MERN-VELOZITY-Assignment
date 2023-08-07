15485f21adbac7d4137677ff4da48e5ebea34ae7
Md Talib Ansari <95065465+talib808@users.noreply.github.com>
2023-08-07 17:41:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,14 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors(
+{
+origin: ["https://deploy-mern-1whq.vercel.app"],
+methods: ["POST", "GET"],
+credentials: true
+}
+));
+// app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
